671921d2637a0dc473d2edf7e42e2aa6b8457a0e
Luis Serra Jiménez <serra.anonym0us@gmail.com>
2018-09-05 23:29:09 +0900

    Primer Problema y inicio de los menus

---
+++ Project-Euler/MultiplosTresYCinco.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Euler
+{
+    class MultiplosTresYCinco
+    {
+        int NumeroFinal;
+
+        public MultiplosTresYCinco(int num)
+        {
+            NumeroFinal = num;
+        }
+
+        public void Multiplos()
+        {
+            Console.Clear();
+            for(int i = 0; i <= NumeroFinal; i++)
+            {
+                if ((i % 3 == 0) || (i % 5) == 0)
+                    Console.Write(i + ", ");
+            }
+        }
+    }
+}
--- Project-Euler/Program.cs
+++ Project-Euler/Program.cs
@@ -8,8 +8,34 @@ namespace Project_Euler
 {
     class Program
     {
+        static int left, top, opcion, numMax;
+
         static void Main(string[] args)
         {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            int sw = 0;
+            Console.Write("Seleccione la opción que desea realizar: ");
+            left = Console.CursorLeft;
+            top = Console.CursorTop;
+            Console.WriteLine("\n 1. Multiplos de 3 y 5");
+
+            Console.SetCursorPosition(left, top);
+            sw = int.Parse(Console.ReadLine());
+
+            switch (sw)
+            {
+                case 1:
+                    Console.Write("\n\nHasta que número quiere obtener: ");
+                    numMax = int.Parse(Console.ReadLine());
+                    MultiplosTresYCinco mtlp = new MultiplosTresYCinco(numMax);
+                    mtlp.Multiplos();
+                    break;
+            }
+
         }
     }
 }
